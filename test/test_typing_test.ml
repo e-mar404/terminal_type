1cf3e6a1328998418d3f78a3529d0ac6e62092a6
emar <egmarinn@gmail.com>
2024-11-25 00:52:34 -0600

    alternating correct and incorrect input

--- test/test_typing_test.ml
+++ test/test_typing_test.ml
@@ -73,12 +73,58 @@ let two_incorrect_input _ =
 
   assert_equal expected_output (Buffer.contents output_buffer)
 
+let one_correct_one_incorrect_input _ = 
+  let input = ref ['t'; 'b'] in
+  let read_input () = 
+    match !input with 
+    | [] -> None
+    | letter :: rest ->
+        input := rest;
+        Some letter
+  in
+
+  let test_text = ['t'; 'e'] in
+
+  let output_buffer = Buffer.create 16 in
+  let display colored_input _ =
+    Buffer.add_string output_buffer colored_input;
+  in
+
+  let _ = typing_test ~read_input ~display ~test_text in
+  let expected_output = "\033[32mt\033[0m\033[31me\033[0m" in
+
+  assert_equal expected_output (Buffer.contents output_buffer)
+
+let one_incorrect_one_correct_input _ = 
+  let input = ref ['a'; 'e'] in
+  let read_input () = 
+    match !input with 
+    | [] -> None
+    | letter :: rest ->
+        input := rest;
+        Some letter
+  in
+
+  let test_text = ['t'; 'e'] in
+
+  let output_buffer = Buffer.create 16 in
+  let display colored_input _ =
+    Buffer.add_string output_buffer colored_input;
+  in
+
+  let _ = typing_test ~read_input ~display ~test_text in
+  let expected_output = "\033[31mt\033[0m\033[32me\033[0m" in
+
+  assert_equal expected_output (Buffer.contents output_buffer)
+
 let suite =
   "suite" >::: [
     "one_correct_input" >:: one_correct_input;
     "one_incorrect_input" >:: one_incorrect_input;
     "two_correct_input" >:: two_correct_input;
     "two_incorrect_input" >:: two_incorrect_input;
+    "one_correct_one_incorrect_input" >:: one_correct_one_incorrect_input;
+    "one_incorrect_one_correct_input" >:: one_incorrect_one_correct_input;
   ]
 
 let () =
